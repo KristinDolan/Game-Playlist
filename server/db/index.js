5983b8f2420d9944339da1e80181a6d8ecd5d61b
Kristin Dolan <95385537+KristinDolan@users.noreply.github.com>
2024-08-15 12:47:28 -0400

    set up db

---
+++ server/db/index.js
@@ -0,0 +1,57 @@
+const Sequelize = require('sequelize');
+
+const dbConnection = new Sequelize(
+    "postgres://postgres:Frostine.13@localhost:5432/gamesplaylist"
+);
+
+module.exports = dbConnection;
+
+/*
+MODELS:
+
+    Games:
+    - title (not null)
+    - rating
+    - played (bool Y/N, default False)
+
+    Genre:
+    - name (not null)
+
+    Many-to-Many movies+genres
+    belongsToMany
+
+*/
+
+const Game = dbConnection.define('game', {
+    title: {
+        type: Sequelize.DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    my_rating: {
+        type: Sequelize.DataTypes.NUMBER,
+        allowNull: true
+    },
+    ign_rating: {
+        type: Sequelize.DataTypes.NUMBER,
+        allowNull: true
+    },
+    ign_review: {
+        type: Sequelize.DataTypes.STRING(255),
+        allowNull: true
+    },
+    played: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+})
+
+const Genre = dbConnection.define("genre", {
+    name: {
+        type: Sequelize.DataTypes.STRING(50),
+        allowNull: false
+    }
+})
